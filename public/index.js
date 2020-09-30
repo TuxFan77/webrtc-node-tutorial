33c1b560be2d4aae85c1db678ea88ebedb42d60d
TuxFan77 <briancross@shaw.ca>
2020-09-29 18:38:06 -0600

    Added button handlers

--- public/index.js
+++ public/index.js
@@ -1,5 +1,24 @@
-navigator.mediaDevices
-  .getUserMedia({ audio: true, video: true })
-  .then(
-    (stream) => (document.querySelector(".local-video").srcObject = stream)
-  );
+const startButton = document.querySelector("#start-btn");
+const endButton = document.querySelector("#end-btn");
+const localVideo = document.querySelector(".local-video");
+
+endButton.disabled = true;
+
+startButton.addEventListener("click", () => {
+  startButton.disabled = true;
+  endButton.disabled = false;
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then(stream => {
+      console.log(stream);
+      localVideo.srcObject = stream;
+    });
+});
+
+endButton.addEventListener("click", () => {
+  const tracks = localVideo.srcObject.getTracks();
+  tracks.forEach(track => track.stop());
+  localVideo.srcObject = null;
+  startButton.disabled = false;
+  endButton.disabled = true;
+});
