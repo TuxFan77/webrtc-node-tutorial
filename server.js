af846ef2de304c831ef268fc72bf46b491c2e56f
TuxFan77 <briancross@shaw.ca>
2020-10-12 21:57:34 -0600

    Removed production environment check for serving static files

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-if (process.env.NODE_ENV === "production")
-  app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
