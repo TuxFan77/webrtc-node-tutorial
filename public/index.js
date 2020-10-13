92938db96f294dbcb8219e3d6bda8f09629f765c
TuxFan77 <briancross@shaw.ca>
2020-10-13 09:48:34 -0600

    Added Socket.io test code

--- public/index.js
+++ public/index.js
@@ -4,15 +4,19 @@ const localVideo = document.querySelector(".local-video");
 
 endButton.disabled = true;
 
-startButton.addEventListener("click", () => {
+const socket = io();
+socket.on("gotmedia", msg => console.log("Other peer got media: ", msg));
+socket.on("end", msg => console.log("Other peer ended call: ", msg));
+
+startButton.addEventListener("click", async () => {
   startButton.disabled = true;
   endButton.disabled = false;
-  navigator.mediaDevices
-    .getUserMedia({ video: true, audio: false })
-    .then(stream => {
-      console.log(stream);
-      localVideo.srcObject = stream;
-    });
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false,
+  });
+  localVideo.srcObject = stream;
+  socket.emit("gotmedia", "Got webcam feed");
 });
 
 endButton.addEventListener("click", () => {
@@ -21,4 +25,5 @@ endButton.addEventListener("click", () => {
   localVideo.srcObject = null;
   startButton.disabled = false;
   endButton.disabled = true;
+  socket.emit("end", "End call");
 });
--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+io.on("connection", socket => {
+  console.log(`User ${socket.id} connected`);
+
+  socket.on("gotmedia", msg => {
+    socket.broadcast.emit("gotmedia", msg);
+  });
+
+  socket.on("end", msg => {
+    socket.broadcast.emit("end", msg);
+  });
+
+  socket.on("disconnect", () => console.log(`User ${socket.id} disconnected`));
+});
